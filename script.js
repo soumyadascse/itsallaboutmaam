51cb7a530bc51fa2405e044e9df09cf5fca45d4b
soumyadascse <87638391+soumyadascse@users.noreply.github.com>
2026-09-09 20:03:19 +0530

    Add heart creation and animation functionality

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+const heartsContainer = document.querySelector(".hearts");
 
+function createHeart() {
+    const heart = document.createElement("div");
+
+    heart.classList.add("floating-heart");
+
+    heart.innerHTML = "♥";
+
+    heart.style.left = Math.random() * 100 + "vw";
+
+    heart.style.fontSize =
+        (12 + Math.random() * 20) + "px";
+
+    heart.style.animationDuration =
+        (4 + Math.random() * 4) + "s";
+
+    heartsContainer.appendChild(heart);
+
+    setTimeout(() => {
+        heart.remove();
+    }, 8000);
+}
+
+setInterval(createHeart, 700);
